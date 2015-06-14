fed9078a408ee06652241c0d4fae5e1c85cb4afd
youtarot <youtarot@h-t.co.jp>
2015-06-14 17:19:48 +0900

    Modify cucumber feature.

--- src/cucumber/resources/steps/ExampleSteps.groovy
+++ src/cucumber/resources/steps/ExampleSteps.groovy
@@ -2,12 +2,3 @@ import cucumber.api.PendingException
 
 import static cucumber.api.groovy.JA.*
 
-前提(~/^トップページにいて$/) { ->
-  // Write code here that turns the phrase above into concrete actions
-  throw new PendingException()
-}
-
-もし(~/^ユーザーID"(.*?)"パスワード"(.*?)"を入力して、ログインすると$/) { String arg1, String arg2 ->
-  // Write code here that turns the phrase above into concrete actions
-  throw new PendingException()
-}
